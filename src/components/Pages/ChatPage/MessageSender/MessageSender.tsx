117b5ba6c11dd2f66e559ac0b7a3cfa4573db13f
skate090961 <m4xwork@yandex.ru>
2023-12-07 18:24:36 +0400

    add react-hook-form on message sender

--- src/components/Pages/ChatPage/MessageSender/MessageSender.tsx
+++ src/components/Pages/ChatPage/MessageSender/MessageSender.tsx
@@ -1,42 +1,43 @@
-import React, {ChangeEvent, memo, useState} from 'react';
+import React, {memo} from 'react'
 import s from './MessageSender.module.css'
-import {useAppDispatch} from "../../../../store/store";
-import {sendMessageTC} from "../../../../store/message/thunk-message";
+import {useAppDispatch} from "../../../../store/store"
+import {SubmitHandler, useForm} from "react-hook-form"
+import {sendMessageTC} from "../../../../store/message/thunk-message"
 
-const MessageSender = ({senderId}: {senderId: string | undefined}) => {
+const MessageSender = ({senderId}: { senderId: string | undefined }) => {
     const dispatch = useAppDispatch()
-    const [value, setValue] = useState<string>('')
-    const [isDisabled, setIsDisabled] = useState<boolean>(false)
-    const changeValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setValue(e.currentTarget.value)
-    }
-    const sendMessageHandler = () => {
-        setIsDisabled(true)
-        dispatch(sendMessageTC(Number(senderId), value))
-            .then(() => {
-                setValue('')
-            })
-            .finally(() => {
-                setIsDisabled(false)
-            })
-    }
+    const {
+        register,
+        handleSubmit,
+        formState,
+        reset,
+        formState: {errors, isValid},
+    } = useForm<{ message: string }>({
+        mode: "onBlur"
+    })
+    const onSubmit: SubmitHandler<{ message: string }> = (data) =>
+        dispatch(sendMessageTC(Number(senderId), data.message))
+            .then(() => reset())
 
     return (
-        <div className={s.sender__form}>
+        <form className={s.sender__form} onSubmit={handleSubmit(onSubmit)}>
             <input
-                value={value}
+                autoFocus
+                type={'text'}
                 className={s.sender__input}
                 placeholder={'Type your message here'}
-                onChange={changeValueHandler}
-                disabled={isDisabled}
+                disabled={formState.isSubmitting}
+                {...register("message", {
+                    validate: value => value.length >= 1
+                })}
             />
             <button
+                type="submit"
                 className={s.sender__button}
-                onClick={sendMessageHandler}
-                disabled={!value || isDisabled}
+                disabled={!isValid || formState.isSubmitting}
             >Send
             </button>
-        </div>
+        </form>
     );
 };
 
